0b940ffc9f4643d7120425ac34a50a3d87ccd5af
rakugun <coodar@gmail.com>
2025-04-19 10:59:45 +0800

    refactor(setup.py): 修复 `find_packages` 函数的参数格式
    将 `find_packages('src')` 修改为 `find_packages(where='src')`，以符合最新 `setuptools` 的 API 要求，避免潜在的兼容性问题

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="deepseek_chat",
     version="0.1.0",
-    packages=find_packages('src'),
+    packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[],
     author="coodar",
