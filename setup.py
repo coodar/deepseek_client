ddb6e474c32ea9d9adc82875ef0c886bda3f297b
rakugun <coodar@gmail.com>
2025-04-19 19:56:25 +0800

    chore: 更新版本号至0.1.1
    为了发布新版本，将setup.py中的版本号从0.1.0更新至0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="deepseek_chat",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=['requests', 'urllib3<2.0'],
